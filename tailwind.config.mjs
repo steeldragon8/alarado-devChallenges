5cd88585905881615e203647a1597c4a61ea0657
Angel Lopez <aabrahamlopez@gmail.com>
2024-01-01 06:40:27 -0600

    Agregando reto business blog card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,7 @@ export default {
 		extend: {
 			fontFamily: {
 				poppins: ['Poppins', 'sans-serif'],
+				lato: ['Lato', 'sans-serif'],
 			},
 			colors: {
 				customBlue: '#2A4DD0',
@@ -14,13 +15,14 @@ export default {
 				customDarkGray: '#223344',
 				customGray: '#909193',
 				customLightGray: '#F2F9FE',
+				primary: '#111729',
+				secondary: '#677489',
+				background: '#F2F5F9',
 			},
 			backgroundImage: {
 				'sun': 'url("../assets/Sun_fill.svg")',
 				'moon': 'url("../assets/Moon_fill.svg")',
 				'moon-light': 'url("../assets/Moon_fill_light.svg")',
-				/* 'close': 'url("../assets/icon-close.svg")',
-				'open': 'url("../assets/icon-open.svg")', */
 			}
 		},
 	},
